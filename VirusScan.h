2f5614c4af9708ffb338491033ebd6441ec0d3d6
Drew <77373359+AjaxSelectButtonGames@users.noreply.github.com>
2024-09-03 12:24:39 -0400

    Update VirusScan.h

--- VirusScan.h
+++ VirusScan.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "globals.h"
 
-const std::string VIRUSTOTAL_API_KEY = "6dfbbf7f2f70a6097bd4b9c8df55d1a822f40cb2d814506c85deb2593cbb5329";
+const std::string VIRUSTOTAL_API_KEY = "INSERT YOUR API KEY HERE";
 
 // Scan a file with VirusTotal
 bool scan_file_with_virustotal(const std::string& filename);
